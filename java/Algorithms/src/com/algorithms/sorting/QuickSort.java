358c39c81d66af8791e7544b8438d77f9b89e36f
Asmita Metrewar <metrewar.asmita@gmail.com>
2016-04-28 15:39:14 +0530

    Quick sort with median as pivot

--- java/Algorithms/src/com/algorithms/sorting/QuickSort.java
+++ java/Algorithms/src/com/algorithms/sorting/QuickSort.java
@@ -16,24 +16,28 @@ public class QuickSort extends Sort {
     private int partition (int array[], int low, int high) {
         int pivotPosition = low + (high-low)/2;
         int pivot = array[pivotPosition];
-        swap(array, pivotPosition, high);
-        int i = low-1;
-        for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
+        
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < pivot)
                 i++;
+            while (array[j] > pivot)
+                j--;
+            if (i <= j) {
                 swap(array, i, j);
+                i++;
+                j--;
             }
         }
-        swap(array, i+1, high);
-        return i+1;
+        return i;
     }
     
     private void quickSort(int array[], int low, int high) {
-        if (low < high) {
-            int partitionIndex = partition(array, low, high);
+        int partitionIndex = partition(array, low, high);
+        if (low < partitionIndex-1)
             quickSort(array, low, partitionIndex-1);
-            quickSort(array, partitionIndex+1, high);
-        }
+        if (partitionIndex < high)
+            quickSort(array, partitionIndex, high);
     }
     
     @Override
